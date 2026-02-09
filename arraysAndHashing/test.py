8ceed128e1b00209de26a0fa0b8a309784fa906a
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-09 20:47:32 -0300

    testing two pointers

--- arraysAndHashing/test.py
+++ arraysAndHashing/test.py
@@ -1,5 +1,15 @@
-thiago = "thiago medeiros barroso"
-if "thiago " in thiago:
-    print("ok")
-else:
-    print("no")
+thiago = "thiago jose de almeida barroso medeiros"
+l, r = 0, 0
+res = ""
+
+while r < len(thiago):
+    if thiago[r] != " ":
+        r += 1
+    else:
+        res += thiago[l:r+1][::-1]
+        r += 1
+        l = r
+res += " "
+res += thiago[l:r+1][::-1]
+    
+print(res[1:])
