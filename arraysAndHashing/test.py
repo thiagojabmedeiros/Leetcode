23605e64920e79e0ac33d013ee1792e804d40a96
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-13 17:15:32 -0300

    .

--- arraysAndHashing/test.py
+++ arraysAndHashing/test.py
@@ -1,15 +1,18 @@
-thiago = "thiago jose de almeida barroso medeiros"
-l, r = 0, 0
-res = ""
-
-while r < len(thiago):
-    if thiago[r] != " ":
-        r += 1
+def quicksort(nums):
+    if len(nums) < 2:
+        return nums
     else:
-        res += thiago[l:r+1][::-1]
-        r += 1
-        l = r
-res += " "
-res += thiago[l:r+1][::-1]
-    
-print(res[1:])
+        print(nums)
+        pivot = nums[0]
+        less = []
+        greater = []
+        for i in nums[1:]:
+            if i <= pivot:
+                less.append(i)
+            else:
+                greater.append(i)
+                
+            print(f"less:{less} + pivot:[{pivot}] + greater:{greater}")
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+print(quicksort([4,12,43,6,2,9,0,5]))
