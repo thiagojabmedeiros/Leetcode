ed83e8f74087899c85b882991dd45a77c6b1ddbc
Thiago Medeiros <thiagojabm@gmail.com>
2026-02-13 18:13:14 -0300

    product except self

---
+++ arraysAndHashing/productsOfArrayExceptSelf.py
@@ -0,0 +1,17 @@
+def productExceptSelf(nums):
+    res = [1] * len(nums)
+
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    
+    postfix = 1
+    for i in range(len(nums) -1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+
+    return res
+
+numsx = [1,2,4,6]
+print(productExceptSelf(numsx))
