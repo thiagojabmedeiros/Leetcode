b5d8cfe035b2450216cef8367246ee3c6c382eb6
Thiago Medeiros <thiagojabm@gmail.com>
2026-03-06 19:10:03 -0300

    .

--- arraysAndHashing/longestConsecutiveSequence.py
+++ arraysAndHashing/longestConsecutiveSequence.py
@@ -1,17 +1,42 @@
-def longestConsecutiveSequence(nums):
-    num_set = set(nums)
-    sequence = 0
 
-    for i in num_set:
-        if (i - 1) not in num_set:
-            length = 1
-            current = i
-            while current + 1 in num_set:
-                current += 1
-                length += 1
-            sequence = max(sequence, length)
-    return sequence
+def longestsequence(nums):
+    snums = set(nums)
+    i = 0
+    print(snums)
+    while i < len(snums):
+        print(snums)
+        i+= 1
+    print(i)
+
+numsx = [2,20,4,10,3,4,5]
+print(longestsequence(numsx))
 
 
-numsx = [0,3,2,5,4,6,1,1]
-print(longestConsecutiveSequence(numsx))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def longestConsecutiveSequence(nums):
+#     num_set = set(nums)
+#     sequence = 0
+
+#     for i in num_set:
+#         if (i - 1) not in num_set:
+#             length = 1
+#             current = i
+#             while current + 1 in num_set:
+#                 current += 1
+#                 length += 1
+#             sequence = max(sequence, length)
+#     return sequence
